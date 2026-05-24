da255295e00e7658a4bc9fa714426f69a3e5a11d
Rehit <rehitsaikrishna@gmail.com>
2026-05-24 22:53:58 +0530

    Created

---
+++ Discussion.sh
@@ -0,0 +1,7 @@
+Persion1= Revanth
+Persion2=Rehit
+
+echo "$Persion1: Hello Rehit."
+echo "$persion2: Hello Revanth."
+echo "$Persion1: How are you"
+echo "$Persion2: I am fine, How about you"
---
+++ conversation.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Revanth: Hi,Rehit"
+echo "Rehit: Hello Revanth"
